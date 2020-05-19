2ee1379151de78376b706771de17e9c81774a646
Phat Ho <hoangphatho@gmail.com>
2020-05-19 20:13:25 +0700

    Update noptien (lam giong taikhoan chu khong biet gi)

--- banking-backend/routes/noptien.route.js
+++ banking-backend/routes/noptien.route.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const nopTienModel = require('../models/noptien.model');
+const taiKhoanModel = require('../models/taikhoan.model');
 const authModel = require('../models/auth.model');
 const moment = require('moment');
 const crypto = require('crypto');
@@ -16,8 +17,8 @@ router.get('/', async (req, res) => {
 
 router.put('/update', async(req, res) => {
     console.log(req.body.SoDu);
-    const list = await nopTienModel.getSoDu(req.body.SoTaiKhoan);
-    var sodu = Number(list[0].SoDu);
+    const listTaiKhoan = await nopTienModel.getSoDu(req.body.SoTaiKhoan);
+    var sodu = Number(listTaiKhoan[0].SoDu);
     if(isNaN(req.body.SoTaiKhoan)){
         return res.status(400).json({
             err: 'Invalid STK'
@@ -25,7 +26,35 @@ router.put('/update', async(req, res) => {
     }
     
     const result = await nopTienModel.update(Number(req.body.SoDu) + sodu, req.body.SoTaiKhoan);
-    res.status(201).json(result);
+    const id = +req.params.id || -1;
+    
+    const ts = +req.headers['time'] || 0;
+    const currentTime = moment().valueOf();
+    const expireTime = 600000; // 10 mins
+
+    if(currentTime - ts > expireTime){
+        const error = 'Loi goi da qua han';
+        return res.json({error});
+    }
+
+    const headerSig = req.headers['sig'] || 0;
+    const sig = crypto.createHash('sha256').update(ts + secretKey).digest('hex');
+    if(sig !== headerSig){
+        const error = 'Goi tin da bi chinh sua';
+        return res.json({error});
+    }
+
+    const code = await authModel.detail(req.headers['bank-code']);
+    if(code.length === 0){
+        const error = 'Ngan hang chua lien ket';
+        return res.json({error});
+    }
+    const list = await taiKhoanModel.detail(id);
+    if(list.length === 0){
+        return res.status(201).json(result);;
+    }
+    res.json(list[0]);
+    
 })
 
 module.exports = router;
