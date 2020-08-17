7e927d1910fade212a741548e5ffbf1332d7bd74
ductai98 <ronin32014@gmail.com>
2020-08-17 22:30:21 +0700

    Link bank 30

--- banking-backend/utils/mailer.js
+++ banking-backend/utils/mailer.js
@@ -8,8 +8,8 @@ const transport = nodemailer.createTransport({
         type: "OAuth2",
         user: config.MAILGUN_USER,
         pass: config.MAILGUN_PASSWORD,
-        accessToken: "ya29.a0AfH6SMDV4PUxcOljs4coNmFScp38AuCZz8Sn50uxOn2frIlJvEElmoPC4G6slgc7ld8zEIes2oYevsV-lk0sj_25zZojhvOELmWHbn2bGfEanJh5A3gNZknG3l20gYIOTNECDdrh5pT-EDGziv7a28FGZRl7feOsQlo",
-        refreshToken: "1//04C5QYVnqQU4MCgYIARAAGAQSNwF-L9Irulg4iIqy8jk-xUR39R1YgFG3CeV35Er1XEOoKlvxucsurc36xEB4sk67PFLJmm2azTY",
+        accessToken: "ya29.a0AfH6SMC8jnFypS5Vwqm-c7GMdkvyhhhQlYgOx8T2IU0yNMWYO1OdarBlpEK7HQld5cbxBLL7NKMSnvvxwQpHFOjU9aM_RJQnxJPUVveqBIDdhD_8vu6TzDivFycY3_8-lGFPvlFvt2prehxfjNCq0dvx3rc1fqoolT8",
+        refreshToken: "1//04sEs2OpM2U_ZCgYIARAAGAQSNwF-L9Ir5mV_roTsSJMq9K1mtgN15PM-nHoGL0YxVwFFEPHpPUmAhBgGwvsjK0GrPkaP5SdRyZw",
         clientId: "350798295911-af0qginemf9jvs1tvpuoquiamh3kt4fk.apps.googleusercontent.com",
         clientSecret: "g7GmqXtK8GMoJz9CFubTmWG2"
     },
